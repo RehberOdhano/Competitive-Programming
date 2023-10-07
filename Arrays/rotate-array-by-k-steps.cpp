73597c69e303960aad09778d458cd907a3cb3fb3
RehberOdhano <rehber.odhano30@gmail.com>
2023-10-07 12:51:41 +0500

    Solved Using Naive Approach - O(n^2): Rotate the given array by k-steps using the constant space (in-place)

---
+++ Arrays/rotate-array-by-k-steps.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// rotating the given array by k-steps using nested loops
+// O(n^2)
+void rotateArrayByKSteps(vector<int>&numbers, int n, int k) {
+	int temp1 = 0, temp2 = 0;
+	// means array is already rotated
+	if(k == 0 || k == n) return;
+
+	// rotating the array to the right by k-steps
+	while(k--) {
+		temp1 = numbers[0];
+		numbers[0] = numbers[n-1];
+		for(int i = 1; i < n; i++) {
+			temp2 = numbers[i];
+			numbers[i] = temp1;
+			temp1 = temp2;
+		}
+	} 
+}
+
+int main() {
+
+	// FAST I/O
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	int t, n = 0, k = 0;
+	cin>>t;
+	while(t--) {
+		cin>>n>>k;
+		vector<int>numbers(n);
+		for(int i = 0; i < n; i++) {
+			cin>>numbers[i];
+		}
+		rotateArrayByKSteps(numbers, n, k);
+		for(int number : numbers) {
+			cout<<number<<" ";
+		}
+		cout<<endl;
+	}
+
+	#ifndef ONLINE_JUDGE
+    	cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+	#endif
+
+	return 0;
+}
